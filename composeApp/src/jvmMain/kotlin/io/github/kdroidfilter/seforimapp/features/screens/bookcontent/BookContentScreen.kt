0dd107a7ffbacb06743993558413b15f1bf2f3f6
Elie Gambache <elyahou.hadass@gmail.com>
2025-07-10 21:29:55 +0300

    feat: Add custom icons and enhance navigation in `BookContentScreen`
    - Implemented new custom icons: `ColorMode`, `Dark_mode`, `Github`, `Hdr_auto`, `Light_mode`, `ListTree`, and `TableOfContents`.
    - Enhanced `BookContentScreen` with dynamic side navigation using `SelectableIconButtonWithTooltip`.
    - Added tooltips and improved layout structure for better user interaction.
    - Updated `EnhancedSplitLayouts` with refined split-pane design and customization.

--- composeApp/src/jvmMain/kotlin/io/github/kdroidfilter/seforimapp/features/screens/bookcontent/BookContentScreen.kt
+++ composeApp/src/jvmMain/kotlin/io/github/kdroidfilter/seforimapp/features/screens/bookcontent/BookContentScreen.kt
@@ -1,26 +1,43 @@
 package io.github.kdroidfilter.seforimapp.features.screens.bookcontent
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.github.kdroidfilter.seforimapp.core.presentation.icons.ListTree
+import io.github.kdroidfilter.seforimapp.core.presentation.icons.TableOfContents
 import org.jetbrains.compose.splitpane.ExperimentalSplitPaneApi
 import org.jetbrains.compose.splitpane.HorizontalSplitPane
 import org.jetbrains.compose.splitpane.SplitPaneState
 import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.component.IconActionButton
+import org.jetbrains.jewel.ui.component.IconButton
+import org.jetbrains.jewel.ui.component.SelectableIconActionButton
 import org.jetbrains.jewel.ui.component.Text
+import org.jetbrains.jewel.ui.component.Tooltip
+import org.jetbrains.jewel.ui.icon.IconKey
+import org.jetbrains.jewel.ui.icons.AllIconsKeys
+import org.jetbrains.jewel.ui.painter.PainterHint
 import org.koin.compose.viewmodel.koinViewModel
 import java.awt.Cursor
+import java.awt.Label
 
 @Composable
 fun BookContentScreen() {
@@ -30,7 +47,7 @@ fun BookContentScreen() {
 
 }
 
-@OptIn(ExperimentalSplitPaneApi::class)
+@OptIn(ExperimentalSplitPaneApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun BookContentView(state: BookContentState, onEvents: (BookContentEvents) -> Unit) {
     // Local scroll state
@@ -68,15 +85,75 @@ fun BookContentView(state: BookContentState, onEvents: (BookContentEvents) -> Un
         }
     }
 
-    EnhancedSplitLayouts(
-        splitPaneState = state.splitPaneState,
-        searchText = state.searchText,
-        onSearchTextChange = { onEvents(BookContentEvents.OnSearchTextChange(it)) },
-        selectedChapter = state.selectedChapter,
-        onChapterSelected = { onEvents(BookContentEvents.OnChapterSelected(it)) },
-        paragraphScrollState = paragraphScrollState,
-        chapterScrollState = chapterScrollState
-    )
+    Row(modifier = Modifier.fillMaxSize()) {
+
+
+        Row(modifier = Modifier.width(70.dp).fillMaxHeight(), horizontalArrangement = Arrangement.Start) {
+            Column {
+                Box(
+                    modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth().padding(4.dp),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+
+                    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                        var isSelected by mutableStateOf(true)
+                        SelectableIconButtonWithToolip(
+                            toolTipText = "Liste des livres",
+                            onClick = {
+                                isSelected = !isSelected
+                            },
+                            isSelected = isSelected,
+                            icon = ListTree,
+                            iconDescription = "",
+                            label = "ספרים"
+                        )
+                        SelectableIconButtonWithToolip(
+                            toolTipText = "Liste des livres",
+                            onClick = {
+                                isSelected = !isSelected
+                            },
+                            isSelected = isSelected,
+                            icon = TableOfContents,
+                            iconDescription = "",
+                            label = "תוכן עניינים"
+                        )
+                    }
+
+
+                }
+                Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(), contentAlignment = Alignment.TopCenter) {
+                    SelectableIconActionButton(
+                        key = AllIconsKeys.General.MoreTabs,
+                        contentDescription = "Linux",
+                        selected = false,
+                        iconModifier = Modifier.size(24.dp),
+                        modifier = Modifier.size(32.dp),
+                        colorFilter = ColorFilter.tint(JewelTheme.globalColors.text.selected),
+                        onClick = {
+
+                        }
+                    )
+                }
+
+            }
+
+        }
+        EnhancedSplitLayouts(
+            modifier = Modifier.fillMaxSize(),
+            splitPaneState = state.splitPaneState,
+            searchText = state.searchText,
+            onSearchTextChange = { onEvents(BookContentEvents.OnSearchTextChange(it)) },
+            selectedChapter = state.selectedChapter,
+            onChapterSelected = { onEvents(BookContentEvents.OnChapterSelected(it)) },
+            paragraphScrollState = paragraphScrollState,
+            chapterScrollState = chapterScrollState
+        )
+    }
+    Row(
+        modifier = Modifier.width(32.dp).fillMaxHeight().background(Color.Red),
+        horizontalArrangement = Arrangement.Start
+    ) {}
+
 }
 
 private fun Modifier.cursorForHorizontalResize(): Modifier =
@@ -85,6 +162,7 @@ private fun Modifier.cursorForHorizontalResize(): Modifier =
 @OptIn(ExperimentalSplitPaneApi::class)
 @Composable
 fun EnhancedSplitLayouts(
+    modifier: Modifier = Modifier,
     splitPaneState: SplitPaneState,
     searchText: String,
     onSearchTextChange: (String) -> Unit,
@@ -93,11 +171,13 @@ fun EnhancedSplitLayouts(
     paragraphScrollState: ScrollState,
     chapterScrollState: ScrollState
 ) {
-    Column(Modifier.fillMaxSize()) {
+    Row(modifier = modifier) {
         HorizontalSplitPane(
             splitPaneState = splitPaneState
         ) {
             first(200.dp) {
+
+
                 // Navigation panel
                 Column(
                     modifier = Modifier
@@ -199,5 +279,51 @@ private fun ChapterItem(
             .padding(8.dp)
     ) {
         Text("Chapitre ${chapter + 1}")
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SelectableIconButtonWithToolip(
+    toolTipText: String,
+    onClick: () -> Unit,
+    isSelected: Boolean,
+    icon : ImageVector,
+    iconDescription : String = "",
+    label: String
+){
+    Tooltip({
+        Text(toolTipText)
+    }) {
+        IconButton(
+            onClick = onClick,
+            modifier = Modifier
+                .background(
+                   if (isSelected) JewelTheme.globalColors.borders.focused else JewelTheme.globalColors.panelBackground,
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(4.dp)
+            ) {
+                Icon(
+                    icon,
+                    iconDescription,
+                    tint = JewelTheme.globalColors.text.selected,
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    label,
+                    color = JewelTheme.globalColors.text.normal,
+                    fontSize = 10.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
