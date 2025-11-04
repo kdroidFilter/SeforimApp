b0ce9f6dd797c58c3e3dc6e066a2a56bed52fcb4
Elie Gambache <elyahou.hadass@gmail.com>
2025-11-04 05:59:05 +0200

    Clean up imports and unused code in `SettingsWindow`
    - Removed redundant imports to streamline the file.
    - Simplified layout-related imports with `.*` usage.
    - Deleted unused utility functions and resource references.
    - Added missing color parameter in vertical `Divider`.

--- SeforimApp/src/jvmMain/kotlin/io/github/kdroidfilter/seforimapp/features/settings/SettingsWindow.kt
+++ SeforimApp/src/jvmMain/kotlin/io/github/kdroidfilter/seforimapp/features/settings/SettingsWindow.kt
@@ -2,23 +2,9 @@ package io.github.kdroidfilter.seforimapp.features.settings
 
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
@@ -31,7 +17,6 @@ import io.github.kdroidfilter.platformtools.OperatingSystem
 import io.github.kdroidfilter.platformtools.getOperatingSystem
 import io.github.kdroidfilter.seforimapp.core.presentation.theme.ThemeUtils
 import io.github.kdroidfilter.seforimapp.core.presentation.theme.ThemeUtils.buildThemeDefinition
-import io.github.kdroidfilter.seforimapp.core.presentation.utils.getCenteredWindowState
 import io.github.kdroidfilter.seforimapp.features.settings.navigation.SettingsNavHost
 import io.github.kdroidfilter.seforimapp.features.settings.ui.SettingsSidebar
 import org.jetbrains.compose.resources.painterResource
@@ -43,21 +28,13 @@ import org.jetbrains.jewel.intui.standalone.theme.default
 import org.jetbrains.jewel.intui.window.decoratedWindow
 import org.jetbrains.jewel.ui.ComponentStyling
 import org.jetbrains.jewel.ui.Orientation
-import org.jetbrains.jewel.ui.component.Text
-import org.jetbrains.jewel.ui.component.Icon
-import org.jetbrains.jewel.ui.component.DefaultButton
-import org.jetbrains.jewel.ui.component.OutlinedButton
-import org.jetbrains.jewel.ui.component.Divider
+import org.jetbrains.jewel.ui.component.*
 import org.jetbrains.jewel.ui.icons.AllIconsKeys
 import org.jetbrains.jewel.window.DecoratedWindow
 import org.jetbrains.jewel.window.TitleBar
 import org.jetbrains.jewel.window.newFullscreenControls
-import seforimapp.seforimapp.generated.resources.Res
-import seforimapp.seforimapp.generated.resources.AppIcon
-import seforimapp.seforimapp.generated.resources.settings
-import seforimapp.seforimapp.generated.resources.settings_cancel
-import seforimapp.seforimapp.generated.resources.settings_ok
- 
+import seforimapp.seforimapp.generated.resources.*
+
 
 @Composable
 fun SettingsWindow(onClose: () -> Unit) {
@@ -133,6 +110,7 @@ private fun SettingsWindowView(
                     // Vertical separator between the menu and content
                     Divider(
                         orientation = Orientation.Vertical,
+                        color = JewelTheme.globalColors.borders.disabled,
                         modifier = Modifier
                             .fillMaxHeight()
                             .padding(horizontal = 8.dp)
